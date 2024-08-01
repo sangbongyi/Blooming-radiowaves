027ee18ec56ae2056ca44ae2dd7b1941c668cb0a
Sangbing_yi <sangbong@me.com>
2024-08-01 16:30:31 +0200

    Add comments and descriptions

--- src/Blooming_radio_waves.cpp
+++ src/Blooming_radio_waves.cpp
@@ -17,30 +17,27 @@
 #include <TEA5767Radio.h>
 
 TEA5767Radio radio = TEA5767Radio();
+AccelStepper stepper1(1, STEP_Pin, DIR_Pin);  // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
 
+// Stepper motor controller pins
 const int DIR_Pin = 10;
 const int STEP_Pin = 11;
 
-//const int Hall_1_Pin = 2;  //up
-const int Hall_2_Pin = 3;  //down
-
-int val_1 = 0;
-int val_2 = 0;
-
-int speed = 200;
-
-volatile int flag_1 = 0;
-volatile int flag_2 = 0;
-
+// Stepper motor position flags
 volatile bool direction = false;  // false = goes up, true = goes down
 volatile bool upward = false;
 volatile bool downward = false;
 
+// Stepper motor variables
 int initial_move_increment = 1;
 int move_increment = 0;
+int speed = 200;
 
-AccelStepper stepper1(1, STEP_Pin, DIR_Pin);  // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+// Hall sensor pin
+const int Hall_2_Pin = 3;  //down
 
+// Hardware interrupt
+// Change flag variables to reverse the direction
 void eventISR_2() {
   //upward = false;
   downward = true;
@@ -49,40 +46,24 @@ void eventISR_2() {
 
 void homeFunction() {
   // Home function set up
-
   stepper1.setMaxSpeed(100.0);      // Set Max Speed of Stepper (Slower to get better accuracy)
   stepper1.setAcceleration(100.0);  // Set Acceleration of Stepper
 
   Serial.println("Start homing process..............");
   Serial.println();
   Serial.println();
-  // CCW // goes up
-  /*Serial.println("Rotating CCW / Increment = -1............");
-  while (digitalRead(Hall_1_Pin) == 1) {  // No contact with the sensor 1 (1 == no contact / 0 == Contact) (Sensor 1 = ??)
-    stepper1.moveTo(initial_move_increment);
-    initial_move_increment--;  // CCW
-    stepper1.run();
-    //Serial.println("Dir B/m_inc= " + String(move_increment));
-    delay(1);
-  }
-  Serial.println("Sensor 1 contacted!");*/
-
-  //stepper1.setCurrentPosition(0);   // Temporal zero position
-  //stepper1.setMaxSpeed(100.0);      // Set Max Speed of Stepper (Slower to get better accuracy)
-  //stepper1.setAcceleration(100.0);  // Set Acceleration of Stepper
-  //initial_move_increment = 1;
 
   // CW // goes down
   Serial.println("Rotating CW / Increment = +1............");
-  while (digitalRead(Hall_2_Pin) == 1) {  // No contact with the sensor 2 (1 == no contact / 0 == Contact) (Sensor 1 = up)
+  while (digitalRead(Hall_2_Pin) == 1) {  // No contact with the sensor 2 (1 == no contact / 0 == Contact)
     stepper1.moveTo(initial_move_increment);
     initial_move_increment++;
     stepper1.run();
     delay(1);
   }
-  Serial.println("Sensor 2 contacted!");
+  Serial.println("HallSensor contacted!");
 
-  stepper1.setCurrentPosition(0);   // Real zero position
+  stepper1.setCurrentPosition(0);   // Set the current position as the start position
   stepper1.setMaxSpeed(1000.0);     // Set Max Speed of Stepper (Slower to get better accuracy)
   stepper1.setAcceleration(500.0);  // Set Acceleration of Stepper
 
@@ -92,23 +73,25 @@ void homeFunction() {
 }
 
 void setup() {
+  // Set serial communication for debugging
   Serial.begin(9600);
-
-  //Radio.init();
+  // Set I2C communication for the radio module
   Wire.begin();
-  radio.setFrequency(93.7);  //On power on go to station 95.2
-
+  // On power-on go to station FM93.7
+  radio.setFrequency(93.7);  
   delay(5);  // Wait for the motor driver to wake up
-
+  // Set the hall sensor pin (HIGH == No contact // LOW == Contact)
   pinMode(Hall_2_Pin, INPUT_PULLUP);
-
+  // Set hardware interrupt
   attachInterrupt(digitalPinToInterrupt(Hall_2_Pin), eventISR_2, FALLING);
-
+  // Set the stepper motor to its home position
   homeFunction();
   stepper1.setSpeed(speed);
 }
 
 void loop() {
+  // Reverse rotation direction when the membrane reaches the top part of the frame
+  // Fully unfolded
   if (move_increment <= -3200) {
     direction = true;
     downward = false;
@@ -117,27 +100,27 @@ void loop() {
     Serial.println();
     delay(5000);
   }
+  // Fully folded
   if (move_increment == 0){
     downward = true;
     direction = false;
-    //delay(5000);  
   } 
 
+  // Set the stepper to runmode
   stepper1.runSpeedToPosition();
-  //delay(10);
-
-  if (direction == true && downward == false) {  // hit the upper sensor. // goes down //cw
+  
+  // Run the stepper motor by its fixed speed
+  if (direction == true && downward == false) {  
     stepper1.moveTo(move_increment);
     stepper1.setSpeed(speed);
-
-    move_increment++;
+    move_increment++; // Increase step until it reaches the target. (Rotation CW / Goes down)
 
     Serial.println("CW= " + String(move_increment));
-  } else if (direction == false && downward == true) {  // hit the down sensor // goes up // ccw
+  } 
+  else if (direction == false && downward == true) {  
     stepper1.moveTo(move_increment);
     stepper1.setSpeed(speed);
-
-    move_increment--;
+    move_increment--; // Decrease step until it reaches the target. (Rotation CCW / Goes up
 
     Serial.println("CCW= " + String(move_increment));  // -3200
   }
